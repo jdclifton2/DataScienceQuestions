4e7bb00e17d28a752b79d0027a49f67cba318344
jdclifton2 <47993826+jdclifton2@users.noreply.github.com>
2020-12-07 21:19:50 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,6 @@
 SELECT a.studentid, a.name, b.total_marks
 FROM name_table as a, mark_table as b
 WHERE a.studentid = b.studentid AND b.total_marks >
-(SELECT total_marks
-FROM mark_table
-where studentid = ‘V002’);
+(SELECT m.total_marks
+FROM mark_table as m
+WHERE studentid = ‘V002’);
